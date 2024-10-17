fb663fb786d0e30eb6993e47545c80ee3dc92683
Nitika Kaushik <nitikakaushik2810@gmail.com>
2024-10-17 15:51:46 +0530

    addition:it is done

--- Calculator.java
+++ Calculator.java
@@ -1,6 +1,10 @@
 import java.util.*;
 public class Calculator{
     public static void main(String[] args){
+int a=10;
+int b=20;
+int c=a+b;
+System.out.println(c);
         System.out.println("welcome");
 }
 }
